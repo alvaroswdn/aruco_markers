daa5842b87dba0065922c3a829126c9abc8e1e03
David Brown <david.brown@inria.fr>
2025-06-12 09:11:15 +0000

    add unit tests

--- aruco_markers/include/aruco_markers/aruco_markers.hpp
+++ aruco_markers/include/aruco_markers/aruco_markers.hpp
@@ -29,6 +29,7 @@ public:
   ArucoMarkersNode();
   void initialize();
   void process_camera_info(const sensor_msgs::msg::CameraInfo & msg);
+  std::string camera_info_topic_;
 
   bool hasReceivedCameraInfo() const
   {
@@ -78,7 +79,6 @@ private:
   double marker_size_;
   std::string camera_frame_;
   std::string image_topic_;
-  std::string camera_info_topic_;
   std::string dictionary_;
   tf2_ros::Buffer tf_buffer_;
   tf2_ros::TransformListener tf_listener_;
